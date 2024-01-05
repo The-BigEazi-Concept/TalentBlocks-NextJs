d6b74b3d1c3c8f154d4b2678f9c7e8820d31f785
DevBigEazi <adesholatajudeen1@gmail.com>
2024-01-05 10:19:17 +0100

    About section completed

---
+++ src/app/components/About.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+const About = () => {
+  return (
+    <section className="bg-neutral-blue py-[76px] px-[120px] flex gap-[89px]">
+      <Image src="/img/about.svg" alt="about" height={549} width={456} />
+      <div className="flex-col justify-center mt-[97px]">
+        <h6 className="text-sm text-orange mb-[12px]">
+          Welcome to TalentsArray
+        </h6>
+        <h2 className="text-[#1c1c1c] text-4xl font-[600] leading-[54px] mb-[24px]">
+          Where Innovation Meets Expertise
+        </h2>
+        <p className="text-sm text-[#555] font-[400] leading-[30px]">
+          At TalentsArray, we recognize the power of hands-on, face-to-face
+          learning. In the ever-evolving landscape of technology, nothing beats
+          the immersive experience of a physical classroom. We're not just an
+          educational institution; we're a community of learners and mentors
+          committed to sculpting the digital leaders of tomorrow.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
